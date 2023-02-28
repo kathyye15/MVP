9b88d4383f4cdf5526c66cab4eda1b25418bb5eb
Kathy Ye <kathyye15@gmail.com>
2023-02-28 01:05:57 -0800

    serving static files

--- server/index.js
+++ server/index.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const app = express();
 const router = require('./router');
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 app.use('/test', router);
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 })
