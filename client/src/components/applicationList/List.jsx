22e9ee8048bf90675313e7eeedd990aac1e8354b
Kathy Ye <kathyye15@gmail.com>
2023-03-02 01:56:09 -0800

    implemented foundational styling

--- client/src/components/applicationList/List.jsx
+++ client/src/components/applicationList/List.jsx
@@ -3,7 +3,16 @@ import ListItem from './ListItem.jsx';
 
 export default function List({ rows }) {
   return (
-    <ul className="applicaion-list">
+    <ul className="application-list">
+      <li style={{display: 'flex'}}>
+        <div style={{display: 'flex', gap: '0.5rem'}}>
+          <div className="col-header">App #</div>
+          <div className="col-header">Name</div>
+          <div className="col-header">Role</div>
+          <div className="col-header">Status</div>
+          <button className="hidden col-header" ><i class="fa-solid fa-pen-to-square" /></button>
+        </div>
+      </li>
       {rows.map((ele, i) => <ListItem item={ele} key={ele.id} />)}
     </ul>
   );
